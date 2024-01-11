4933ef289d383d0a4fda576ce0653e36e1c43e8b
natalia bolozan <bolozan.natalia@gmail.com>
2024-01-11 18:17:09 +0100

    init coomit

--- src/pages/CategoriesPage/index.jsx
+++ src/pages/CategoriesPage/index.jsx
@@ -1,20 +1,39 @@
+// import React from 'react'
+// import { useSelector } from 'react-redux';
+// import CategoryCard from '../../components/CategoryCard';
+// import s from "./style.module.css"
+// import shop from './image/shop.jpg'
+
+// export default function CategoriesPage() {
+// const categories = useSelector(state => state.categories);
+
+//   return (
+//     <div className={s.container}>
+//       <div className={s.item}>
+//       <img src={shop} alt="shop" />
+//           <h1>Shop</h1>
+//       </div>
+//     <div className={s.cardsContainer}>
+//       {
+//         categories.map(item=>{
+//           return <CategoryCard key={item} title={item} />
+//         })
+//       }
+//     </div>
+//     </div>
+//   )
+// }
 import React from 'react'
 import { useSelector } from 'react-redux';
 import CategoryCard from '../../components/CategoryCard';
 import s from "./style.module.css"
-import shop from './image/shop.jpg'
 
 export default function CategoriesPage() {
 const categories = useSelector(state => state.categories);
 
   return (
     <div className={s.container}>
-      <div className={s.item}>
-      <img src={shop} alt="" />
-      {/* <h1><span>What is Lorem Ipsum?</span><br/>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1> */}
-          <h1>Shop.com</h1>
-      </div>
+      <h1>Лучший магазин лучших товаров</h1>
     <div className={s.cardsContainer}>
       {
         categories.map(item=>{
