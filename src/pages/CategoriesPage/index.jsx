070a554244f773636f4b69eeb2cc8d2ca581788a
natalia bolozan <bolozan.natalia@gmail.com>
2024-01-11 18:26:45 +0100

    init commit

--- src/pages/CategoriesPage/index.jsx
+++ src/pages/CategoriesPage/index.jsx
@@ -1,39 +1,19 @@
-// import React from 'react'
-// import { useSelector } from 'react-redux';
-// import CategoryCard from '../../components/CategoryCard';
-// import s from "./style.module.css"
-// import shop from './image/shop.jpg'
-
-// export default function CategoriesPage() {
-// const categories = useSelector(state => state.categories);
-
-//   return (
-//     <div className={s.container}>
-//       <div className={s.item}>
-//       <img src={shop} alt="shop" />
-//           <h1>Shop</h1>
-//       </div>
-//     <div className={s.cardsContainer}>
-//       {
-//         categories.map(item=>{
-//           return <CategoryCard key={item} title={item} />
-//         })
-//       }
-//     </div>
-//     </div>
-//   )
-// }
 import React from 'react'
 import { useSelector } from 'react-redux';
 import CategoryCard from '../../components/CategoryCard';
 import s from "./style.module.css"
+import shop from './image/shop.jpg'
 
 export default function CategoriesPage() {
 const categories = useSelector(state => state.categories);
 
   return (
     <div className={s.container}>
-      <h1>Лучший магазин лучших товаров</h1>
+      <div className={s.item}>
+      <img src={shop} alt="Shop" />
+   
+          <h1>Shop.com</h1>
+      </div>
     <div className={s.cardsContainer}>
       {
         categories.map(item=>{
